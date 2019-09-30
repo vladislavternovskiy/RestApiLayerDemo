83717b605916be3c1a1c861f1ee1ddee39c17057
Vladislav Ternovskiy <vladislav.ternovskii@gmail.com>
2019-09-30 22:02:24 +0300

    Initial Commit

---
+++ RestApiLayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestApiLayerDemo
+//
+//  Created by Владислав Терновский on 9/30/19.
+//  Copyright © 2019 Vladyslav Ternovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
